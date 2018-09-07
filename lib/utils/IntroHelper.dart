ba854784f7da81a1846034a3a6193a404ae37dee
Jack hosking <jackhosking2@outlook.com>
2018-09-07 21:47:05 +1200

    IntroHelper

---
+++ lib/utils/IntroHelper.dart
@@ -0,0 +1,19 @@
+class IntroHelper {
+  static const String name = "Plant A tree";
+  static const String store = "Online Store\n For Everyone";
+  static const String titleCard1 = "WELCOME";
+  static const String textCard1 =
+      "Plant a Tree is \n Platform for Everyone";
+  static const String titleCard2 = "EXPLORE EARTH";
+  static const String textCard2 =
+      "Search the highest quality Tree Products\n With low-low prices";
+  static const String titleCard3 = "Shopping Cart";
+  static const String textCard3 =
+      "Add to your cart the Trees and Products\n Then Checkout the Order";
+  static const String titleCard4 = "VERIFY AND RECEIVE";
+  static const String textCard4 =
+      "We Will Verify Your Order\n And arrange delivery";
+  static const String skip = "SKIP";
+  static const String next = "NEXT";
+  static const String thanks = "Thanks!";
+}
