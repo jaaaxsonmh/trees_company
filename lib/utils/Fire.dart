22b51360f5f179eabfc6f922991cde9e421c72c7
deishelon <deishelon@gmail.com>
2018-09-13 17:40:31 +1200

    Added Tree Detailed info: https://trello.com/c/15APsB2e/10-as-a-user-i-want-to-be-able-to-select-a-tree-from-a-list-so-i-can-view-detailed-information-about-it

--- lib/utils/Fire.dart
+++ lib/utils/Fire.dart
@@ -13,6 +13,14 @@ class Fire {
   static const String TREE_TITLE = "title";
   static const String TREE_IMAGE = "image";
   static const String TREE_PRICE = "price";
+  static const String TREE_CATEGORY = "categoty";
+  static const String TREE_MAX_HEIGHT = "max_height";
+  static const String TREE_GROWTH_RATE = "growth_rate";
+  static const String TREE_MAINTENANCE_REQ = "maintainence_req";
+  static const String TREE_FACTS = "facts";
+  static const String TREE_SOIL_DRAINAGE = "soil_drainage";
+  static const String TREE_WHERE_FROM = "origin";
+  static const String TREE_SUN_REQ = "sun_req";
 
   /// TOOLS FIELDS
   static const String TOOLS_TITLE = "title";
