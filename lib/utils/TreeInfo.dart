d7d8ee2234a72828bc571db4cfcc6986daeb6972
deishelon <deishelon@gmail.com>
2018-09-13 17:59:07 +1200

    Added possible flags for tree info

---
+++ lib/utils/TreeInfo.dart
@@ -0,0 +1,32 @@
+class TreeCategories{
+  static const String category_decorative = "decorative";
+
+}
+class TreeMaxHeight{
+  static const String height_less_1m = "<1m";
+  static const String height_1m_2m = "1-2m";
+  static const String height_2m_3m = "2-3m";
+  static const String height_more_3m = ">3m";
+}
+class TreeGrowthRate{
+  static const String rate_fast = "fast";
+  static const String rate_medium = "medium";
+  static const String rate_slow = "slow";
+}
+class TreeMaintenance{
+  static const String maintenance_fast = "high";
+  static const String maintenance_medium = "medium";
+  static const String maintenance_low = "low";
+}
+class TreeSoilDrainage{
+  static const String soil_drainage_fast = "high";
+  static const String soil_drainage_medium = "medium";
+  static const String soil_drainage_low = "low";
+  static const String soil_drainage_anything = "anything";
+}
+class TreeSun{
+  static const String sun_sunny = "sunny";
+  static const String sun_med = "med";
+  static const String sun_shade = "shade";
+  static const String sun_anything = "anything";
+}
