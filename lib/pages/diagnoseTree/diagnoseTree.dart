98a8401a6febda5c1f72f981db8778fc2d38df55
Jack hosking <jackhosking2@outlook.com>
2018-09-27 22:03:09 +1200

    Changing anchoring, and confirmation message.

--- lib/pages/diagnoseTree/diagnoseTree.dart
+++ lib/pages/diagnoseTree/diagnoseTree.dart
@@ -27,6 +27,59 @@ class _diagnoseTree extends State<DiagnoseTree> {
     setState(() {});
   }
 
+  _onSubmit() {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: new Container(
+              child: new Image.asset("assets/sprout.png",
+              width: 50.0, height: 50.0),
+            ),
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children : <Widget>[
+                Expanded(
+                  child: Text(
+                    'Thank you! \n A response is on its way \n While you wait check this out!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.green,
+
+                    ),
+                  ),
+                )
+              ],
+            ),
+            actions: <Widget>[
+              new Column( children: <Widget>[
+                FlatButton(
+                    child: new Text('Close', style: new TextStyle(color: Colors.white)),
+                    color: Colors.green,
+                    onPressed: () {
+                      //TODO: Send to detailed information of different tree types
+                      Navigator.pop(context);
+                    })
+              ],
+              ),
+                  new Column( children: <Widget>[
+                    FlatButton(
+                        child: new Text('Home', style: new TextStyle(color: Colors.white)),
+                        color: Colors.green,
+                        onPressed: () {
+                          //TODO: Send to detailed information of different tree types page
+                          MyNavigator.goToHome(context);
+                        })
+                  ],)
+            ],
+          );
+        },
+      );
+    }
+
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -57,7 +110,7 @@ class _diagnoseTree extends State<DiagnoseTree> {
                         EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0),
                     child: new Image.file(image, width: 290.0, height: 290.0)),
           ),
-          RaisedButton(
+          new RaisedButton(
             onPressed: _pickImageGallery,
             color: Colors.green,
             /*(){
@@ -73,29 +126,44 @@ class _diagnoseTree extends State<DiagnoseTree> {
             },*/
             child: Icon(Icons.add_a_photo, color: Colors.white),
           ),
-          new TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              // Use email input type for emails.
-              decoration: new InputDecoration(
-                  hintText: 'you@example.com', labelText: 'E-mail Address')),
-          new TextFormField(
-              decoration: new InputDecoration(
-                  hintText: 'Tell us what you think is wrong?',
-                  labelText: 'Enter short details about the tree')),
-          new Container(
-            child: new RaisedButton(
-              child: new Text(
-                'Send for Review',
-                style: new TextStyle(color: Colors.white),
-              ),
-              // TODO: send data from each field and image to firebase
-              //TODO: Show comfirmation of being sent.
-              onPressed: () => null,
-              color: Colors.green,
-            ),
+          new Padding(
+            padding: EdgeInsets.only(left: 5.0, right: 5.0),
+            child: new TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                // Use email input type for emails.
+                decoration: new InputDecoration(
+                    hintText: 'you@example.com', labelText: 'E-mail Address'))
           ),
+          new Padding(
+            padding: EdgeInsets.only(left: 5.0, right: 5.0),
+            child:new TextFormField(
+                decoration: new InputDecoration(
+                    hintText: 'Tell us what you think is wrong?',
+                    labelText: 'Enter short details about the tree'))
+          ),
+            new Container(
+              child: new RaisedButton(
+                child: new Text(
+                  'Send for Review',
+                  style: new TextStyle(color: Colors.white),
+                ),
+                // TODO: send data from each field and image to firebase
+                onPressed: () {
+                  _onSubmit();
+                  //TODO: add send to firebase
+                },
+                color: Colors.green,
+              ),
+            ),
         ],
       ),
+    // this doesnt work since only 1 item, and it requires >= 2.
+    /*  bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.check),
+          title: new Text('Submit')
+        )
+      ]),*/
     );
   }
 }
