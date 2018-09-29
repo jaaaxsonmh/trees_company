8435a879fa0769b1bbcd0bf1991f4c8b53f483d0
deishelon <deishelon@gmail.com>
2018-09-29 15:04:56 +1200

    Added unit testting

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,11 +1,34 @@
 import 'package:test/test.dart';
+import 'package:trees_co/utils/Routers.dart';
 
 void main() {
 
-  test('my first unit test', () {
-    var answer = 42;
-    expect(answer, 42);
-  });
+  test('Routes testing', () {
 
-  
+    var AR_KEY = Routers.AR_KEY;
+    expect(AR_KEY, Routers.AR_KEY);
+
+    var home = Routers.home;
+    expect(home, Routers.home);
+
+    var intro = Routers.intro;
+    expect(intro, Routers.intro);
+
+    var diagnoseTree = Routers.diagnoseTree;
+    expect(diagnoseTree, Routers.diagnoseTree);
+
+    var newsFullInfo = Routers.newsFullInfo;
+    expect(newsFullInfo, Routers.newsFullInfo);
+
+    var settings = Routers.settings;
+    expect(settings, Routers.settings);
+
+    var delivery = Routers.delivery;
+    expect(delivery, Routers.delivery);
+
+    var treeCare = Routers.treeCare;
+    expect(treeCare, Routers.treeCare);
+
+
+  });
 }
