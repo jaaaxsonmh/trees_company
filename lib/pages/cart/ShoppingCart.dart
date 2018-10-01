637bca9b958744af5d5018913634a66b9f7bd24a
deishelon <deishelon@gmail.com>
2018-10-01 17:30:00 +1300

    Added item QTY to cart

--- lib/pages/cart/ShoppingCart.dart
+++ lib/pages/cart/ShoppingCart.dart
@@ -9,10 +9,8 @@ class ShoppingCart extends StatefulWidget {
   }
 }
 
-class _CartState  extends State<ShoppingCart> {
-
-  Map<String, bool> values = {
-  };
+class _CartState extends State<ShoppingCart> {
+  Map<String, bool> values = {};
 
   int numberOfItemsInShoppingCart = 0;
   int totalPriceOfShoppingCart = 0;
@@ -20,34 +18,31 @@ class _CartState  extends State<ShoppingCart> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      bottomNavigationBar: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: new Text("Total: \$ $totalPriceOfShoppingCart"),
-              ),
-            ),
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: new RaisedButton(
-                    onPressed: pressedBuyButtonn,
-                    textColor: Colors.white,
-                    //minWidth: 50.0,
-                    color: Colors.green,
-                    child: Row(
-                      children: <Widget>[
-                        //Icon(Icons.camera),
-                        new Text('Buy ($numberOfItemsInShoppingCart)'),
-                      ],
-                    )),
-              ) ,
-            ),
-          ]
-
-      ),
+      bottomNavigationBar:
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Text("Total: \$ $totalPriceOfShoppingCart"),
+          ),
+        ),
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new RaisedButton(
+                onPressed: pressedBuyButtonn,
+                textColor: Colors.white,
+                //minWidth: 50.0,
+                color: Colors.green,
+                child: Row(
+                  children: <Widget>[
+                    //Icon(Icons.camera),
+                    new Text('Buy ($numberOfItemsInShoppingCart)'),
+                  ],
+                )),
+          ),
+        ),
+      ]),
       appBar: new AppBar(
           backgroundColor: Colors.green,
           title: Row(
@@ -57,13 +52,14 @@ class _CartState  extends State<ShoppingCart> {
               Text('Shopping cart'),
               new Padding(
                   padding: EdgeInsets.all(10.0),
-                  child:
-                  new Icon(Icons.shopping_basket)
-              )
+                  child: new Icon(Icons.shopping_basket))
             ],
           )),
       body: new StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance.collection(Fire.shoppingCart).orderBy(Fire.SHOPPING_CART_TIME).snapshots(),
+        stream: Firestore.instance
+            .collection(Fire.shoppingCart)
+            .orderBy(Fire.SHOPPING_CART_TIME)
+            .snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData)
             return new Center(
@@ -78,15 +74,15 @@ class _CartState  extends State<ShoppingCart> {
           return new ListView.builder(
               itemCount: snapshot.data.documents.length,
               padding: const EdgeInsets.all(5.0),
-              itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index]));
+              itemBuilder: (context, index) =>
+                  _buildListItem(context, snapshot.data.documents[index]));
         },
       ),
     );
   }
 
   _buildListItem(BuildContext context, DocumentSnapshot document) {
-
-    if (!values.containsKey(document.documentID)){
+    if (!values.containsKey(document.documentID)) {
       values[document.documentID] = false;
     }
 
@@ -97,35 +93,35 @@ class _CartState  extends State<ShoppingCart> {
           children: <Widget>[
             new ListTile(
               key: new ValueKey(document.documentID),
-              leading: new Checkbox(value: values[document.documentID], onChanged: (bool value) {
-                setState(() {
-                  values[document.documentID] = value;
+              leading: new Checkbox(
+                  value: values[document.documentID],
+                  onChanged: (bool value) {
+                    setState(() {
+                      values[document.documentID] = value;
 
-                  if (value){
-                    numberOfItemsInShoppingCart++;
-                    totalPriceOfShoppingCart += document[Fire.SHOPPING_CART_ITEM_PRICE];
-                  }else{
-                    numberOfItemsInShoppingCart--;
-                    totalPriceOfShoppingCart -= document[Fire.SHOPPING_CART_ITEM_PRICE];
-                  }
-                });
-              }),
+                      if (value) {
+                        numberOfItemsInShoppingCart++;
+                        totalPriceOfShoppingCart +=
+                            document[Fire.SHOPPING_CART_ITEM_PRICE];
+                      } else {
+                        numberOfItemsInShoppingCart--;
+                        totalPriceOfShoppingCart -=
+                            document[Fire.SHOPPING_CART_ITEM_PRICE];
+                      }
+                    });
+                  }),
               title: new Text(
                 document[Fire.SHOPPING_CART_ITEM_TITLE],
               ),
-              subtitle: new Text("\$ ${document[Fire.SHOPPING_CART_ITEM_PRICE]}"),
+              subtitle:
+                  new Text("\$ ${document[Fire.SHOPPING_CART_ITEM_PRICE]} | QTY: ${document[Fire.SHOPPING_CART_ITEM_QUANTITY]}"),
               //onTap: () => _openNewsFullPage(context, document),
             ),
           ],
         ),
       ),
     );
-
   }
 
-  void pressedBuyButtonn() {
-
-  }
-
-
+  void pressedBuyButtonn() {}
 }
