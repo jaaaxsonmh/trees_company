938e7f502e8a3bcd3652022064a033a3adda163c
Jack hosking <jackhosking2@outlook.com>
2018-10-02 20:25:07 +1300

    abstraction

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Auth {
+abstract class BaseAuth {
+  Future<String> signInWitEmailAndPassword(String email, String password);
+}
+
+class Auth implements BaseAuth {
     Future<String> signInWitEmailAndPassword(String email, String password) async{
       FirebaseUser user = await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: password);
