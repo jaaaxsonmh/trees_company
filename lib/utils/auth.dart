dc5d6d4bb104973bfba0e5d893a0c0a68135627b
Jack hosking <jackhosking2@outlook.com>
2018-10-02 20:24:11 +1300

    return future string

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -2,9 +2,10 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Auth {
-    void signInWitEmailAndPassword(String email, String password) async{
+    Future<String> signInWitEmailAndPassword(String email, String password) async{
       FirebaseUser user = await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: password);
+      return user.uid;
     }
 }
 
