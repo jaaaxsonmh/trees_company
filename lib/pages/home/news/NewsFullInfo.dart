21ab7eb08e06b075e552d40db6f0a5a379b18bb8
deishelon <deishelon@gmail.com>
2018-10-04 14:22:47 +1300

    removed back bitton

--- lib/pages/home/news/NewsFullInfo.dart
+++ lib/pages/home/news/NewsFullInfo.dart
@@ -66,7 +66,10 @@ class _NewsFullInfo extends State<NewsFullInfo>
             buildTreeCoLogo(),
             new Divider(color: Colors.green),
             buildCardView(newsTimeFormatted),
-            buildBackButton(context),
+            Expanded(
+              flex:1,
+                  child: new Text('')
+            )
           ],
         ),
       ),
@@ -116,20 +119,5 @@ class _NewsFullInfo extends State<NewsFullInfo>
     );
   }
 
-  Expanded buildBackButton(BuildContext context) {
-    return Expanded(
-        flex: 1,
-        child: Row(
-          children: <Widget>[
-            FlatButton(
-              child: Text('Back',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0)),
-              onPressed: () => Navigator.pop(context),
-            ),
-          ],
-        ));
-  }
+
 }
